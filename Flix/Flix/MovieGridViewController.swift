05640980b0200b90c961d4c1045a374ff6831485
Daniel Ochoa Aguila <ocho2088@m506-118-26438.local>
2019-02-04 15:05:25 -0800

    working on unit 2

---
+++ Flix/Flix/MovieGridViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Daniel Ochoa Aguila on 2/4/19.
+//  Copyright © 2019 Daniel Ochoa Aguila. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
