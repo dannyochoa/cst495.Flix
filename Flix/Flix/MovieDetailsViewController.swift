ab28f7efa28663bb6cc7e98a60f9241d751c15b4
Daniel  Ochoa Aguila <danielochoaaguila@daniels-mbp.attlocal.net>
2019-02-06 00:53:34 -0800

    extra credit

--- Flix/Flix/MovieDetailsViewController.swift
+++ Flix/Flix/MovieDetailsViewController.swift
@@ -44,15 +44,21 @@ class MovieDetailsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        performSegue(withIdentifier: "videoSegue", sender: nil)
+    }
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let trailerViewController = segue.destination as! trailerViewController
+        let tempKey = movie["id"] ?? ""
+        trailerViewController.key = tempKey
     }
-    */
-
 }
